94f6a637b221d5aad35b7192729ebc1f4418c419
ton <sudto34@hotmail.com>
2016-11-23 19:51:57 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'StMQQmUud2pB5f5h2RqjfdOnwO+zrrwsVGJWAkQ6K/+RBVJAHMps/OcPFQ65LcGKMhRRfbMRIBWKp9pELtIxjEg8lK5ol2SAobl8Drfg30y4aSXQtcvo5woZwet8WEXLVRDjMD+us6viLi+YEPr2LAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'cxvcc';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
