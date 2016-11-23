c126bc59a6807a644f9891d4c39b86bbd0036f94
ton <sudto34@hotmail.com>
2016-11-23 13:10:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text.'--curl_init'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
